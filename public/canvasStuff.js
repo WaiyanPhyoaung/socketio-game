809ece2fd43a542764f01f6978754d20d90865f0
Waiyan Phyoaung <waiyanphyoaung2015@gmail.com>
2025-03-31 21:48:19 +0630

    fix: add log line

--- public/canvasStuff.js
+++ public/canvasStuff.js
@@ -9,6 +9,7 @@ player.locY = Math.floor(500 * Math.random() + 10);
 // ============Draw============
 
 function draw() {
+  console.log("drawing");
   context.clearRect(0, 0, canvas.width, canvas.height);
   context.setTransform(1, 0, 0, 1, 0, 0);
 
